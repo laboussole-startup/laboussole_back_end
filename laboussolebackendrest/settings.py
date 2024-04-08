1e6d216dc7b894befb044efa0f819e2f4766e793
AristideAbeng <43416631+AristideAbeng@users.noreply.github.com>
2024-04-08 16:55:39 +0100

    Update settings.py

--- laboussolebackendrest/settings.py
+++ laboussolebackendrest/settings.py
@@ -88,7 +88,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'laboussolebackendrest.urls'
