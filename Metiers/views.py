59ad4930eea40291de4bdb37b128e3609a5ba001
AristideAbeng <43416631+AristideAbeng@users.noreply.github.com>
2024-04-12 12:05:51 +0100

    Update views.py

--- Metiers/views.py
+++ Metiers/views.py
@@ -35,8 +35,24 @@ class MetiersListView(generics.GenericAPIView):
                 conditions |= Q(nom__icontains=word)
             # Apply the filter
             queryset = queryset.filter(conditions)
-        # Sort the queryset alphabetically by the 'nom' field
-        queryset = queryset.order_by('nom')
+            # Annotate queryset with count of words from search query found in nom field
+            queryset = queryset.annotate(
+                word_count=Sum(
+                    Case(
+                        When(
+                            *[Q(nom__icontains=word) for word in search_words],
+                            then=Value(1),
+                        ),
+                        default=Value(0),
+                        output_field=models.IntegerField(),
+                    )
+                )
+            )
+            # Sort the queryset by the number of words found in descending order
+            queryset = queryset.order_by('-word_count', 'nom')
+        else:
+            # If search parameter is None, order by ascending order of nom field
+            queryset = queryset.order_by('nom')
         return queryset
 
     def get(self,request,*args,**kwargs):
