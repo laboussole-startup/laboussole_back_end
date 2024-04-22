35ad191edcc4874a1e487582bdd9b3c3a0f9a32c
Paul <abengaristide@gmail.com>
2024-04-22 11:46:48 +0100

    latest changes

--- Metiers/urls.py
+++ Metiers/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('',views.MetiersListView.as_view(),name='metier list view'),
-    path('<int:metier_id>/',views.MetiersDetailView.as_view(),name='metier detail view')
+    path('<int:metier_id>/',views.MetiersDetailView.as_view(),name='metier detail view'),
+    path('reccomendations/',views.MetierRecommendationsView.as_view(),name='metier recommend view')
 ]
--- Metiers/views.py
+++ Metiers/views.py
@@ -119,3 +119,66 @@ class MetiersDetailView(generics.GenericAPIView):
         metiers.delete()
 
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class MetierRecommendationsView(generics.GenericAPIView):
+
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    serializer_class = serializers.MetiersCreationSerializer
+
+    queryset = Metiers.objects.all()
+
+    pagination_class = PageNumberPagination
+
+    def get_queryset(self):
+        queryset = Metiers.objects.all()
+        search_query = self.request.query_params.get('search', None)
+        if search_query:
+            # Split the search query into individual words
+            search_words = search_query.split()
+            # Create a Q object to combine multiple OR conditions
+            conditions = Q()
+            for word in search_words:
+                # Add OR condition for each word in the search query
+                conditions |= (
+                    Q(titre__unaccent__icontains=word) |
+                    Q(description__unaccent__icontains=word) |
+                    Q(competencescles__unaccent__icontains=word) |
+                    Q(principales_missions__unaccent__icontains=word)
+                )
+            # Apply the filter
+            queryset = queryset.filter(conditions)
+            # Annotate queryset with count of words from search query found in specified fields
+            queryset = queryset.annotate(
+                word_count=Sum(
+                    Case(
+                        [
+                            When(titre__unaccent__icontains=word, then=Value(1)),
+                            When(description__unaccent__icontains=word, then=Value(1)),
+                            When(competencescles__unaccent__icontains=word, then=Value(1)),
+                            When(principales_missions__unaccent__icontains=word, then=Value(1)),
+                        ],
+                        default=Value(0),
+                        output_field=IntegerField(),
+                    )
+                )
+            )
+            # Sort the queryset by the number of words found in descending order
+            queryset = queryset.order_by('-word_count', 'titre')
+        else:
+            # If search parameter is None, order by ascending order of titre field
+            queryset = queryset.order_by('titre')
+        return queryset
+
+    def get(self,request,*args,**kwargs):
+        queryset = self.get_queryset()
+        queryset = self.filter_queryset(self.get_queryset())
+        page = self.paginate_queryset(queryset)  # Paginate the queryset
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+        
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(data=serializer.data,status=status.HTTP_200_OK)
+        
