48ea76c4c27d61d6d893ef869904af1728553300
Paul <abengaristide@gmail.com>
2024-04-29 07:28:45 +0100

    latest changes

--- Temoignage/serializers.py
+++ Temoignage/serializers.py
@@ -7,11 +7,10 @@ class TemoignageCreationSerializer(serializers.ModelSerializer):
     nom = serializers.CharField(max_length=255)
     telephone = serializers.CharField(max_length=20)
     contenu = serializers.CharField()
-    photo = serializers.ImageField()
     profession = serializers.CharField(max_length=255)
     Score = serializers.IntegerField()
 
     class Meta:
 
         model = Temoignage
-        fields = ['id_temoignage','nom','telephone','contenu','profession','photo','Score']
+        fields = ['id_temoignage','nom','telephone','contenu','profession','Score']
