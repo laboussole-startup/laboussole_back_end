771dc31e371b35f6a4a2c83097988a7930edbd20
Paul <abengaristide@gmail.com>
2024-05-01 12:09:04 +0100

    latest changes

--- Metiers/urls.py
+++ Metiers/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('<int:metier_id>/',views.MetiersDetailView.as_view(),name='metier detail view'),
     path('reccomendations/',views.MetierRecommendationsView.as_view(),name='metier recommend view'),
     path('debouches/<str:faculte_id>/', views.FacultesDebouchesView.as_view(), name='debouches_view'),
+    path('debouches/<str:filiere_id>/', views.FilieresDebouchesView.as_view(), name='debouches_view'),
 
 ]
