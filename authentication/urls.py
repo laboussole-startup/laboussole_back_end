9199a7780d76e0dd7b9d5c39936302e8f84d9517
Paul <abengaristide@gmail.com>
2024-05-08 10:30:55 +0100

    latest changes

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('signup/',views.UserCreateView.as_view(),name='signup view'),
     path('<user_email>/', views.UserGetDetailView.as_view(), name='get_detail_user'),
-    path('recover/',views.RecoverPasswordView.as_view(),name='recover'),
+    path('recover/<user_email>/',views.RecoverPasswordView.as_view(),name='recover'),
 
 ]
