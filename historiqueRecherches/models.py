f8d00fbc22bb0925ca4d12e7c1b62a1e7e740d82
Paul <abengaristide@gmail.com>
2024-05-10 23:22:48 +0100

    latest changes

--- historiqueRecherches/models.py
+++ historiqueRecherches/models.py
@@ -4,7 +4,7 @@ from authentication.models import Utilisateur
 # Create your models here.
 
 class Recherches(models.Model):
-    id_utilisateur = models.ForeignKey(Utilisateur, models.DO_NOTHING, db_column='id_utilisateur', blank=True, null=True)
+    id_utilisateur = models.IntegerField(blank=True, null=True)
     date_heure = models.DateTimeField(blank=True, null=True)
     mots_cles = models.CharField(max_length=255, blank=True, null=True)
 
