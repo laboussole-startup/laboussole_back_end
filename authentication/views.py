d76680a6390fb280257b71b87f1dae21c766eccb
Paul <abengaristide@gmail.com>
2024-05-12 04:11:01 +0100

    latest changes

--- authentication/views.py
+++ authentication/views.py
@@ -36,21 +36,9 @@ class UserCreateView(APIView):
 
         return Response(data=serializer.errors ,status=status.HTTP_400_BAD_REQUEST)
 
-class IsOwnerOrReadOnly(permissions.BasePermission):
-    """
-    Custom permission to only allow owners of an object to edit it.
-    """
-    def has_object_permission(self, request, view, obj):
-        # Read permissions are allowed to any request,
-        # so we'll always allow GET, HEAD or OPTIONS requests.
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        # Write permissions are only allowed to the owner of the snippet.
-        return obj.email == request.user.email
 
 class UserGetDetailView(generics.GenericAPIView):
-    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly, IsAdminUser]
+    permission_classes = [IsAuthenticated, IsAdminUser]
     serializer_class = serializers.UserDetailSerializer  # Update to appropriate serializer
 
     def get(self, request, user_email):
